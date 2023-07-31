64f3260e188d496493b2952290b0038c87d381cb
VitorKasper <vitorkasper32@gmail.com>
2023-07-31 10:14:35 -0300

    atualizando o arquivo app js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando com banco de dados")
+// Mais um console.log para testar:
+console.log("Eu adicionei mais um console.log")
