cf0ff8ee7714e8ae712b419eed82bf96dfad8d41
VitorKasper <vitorkasper32@gmail.com>
2023-07-31 10:26:35 -0300

    voltando para trás

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando com banco de dados")
-// Mais um console.log para testar:
-console.log("Eu adicionei mais um console.log")
