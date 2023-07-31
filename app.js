4739be3bd8b1deb880f1c28885fd5d04af949755
VitorKasper <vitorkasper32@gmail.com>
2023-07-31 10:37:59 -0300

    adicionando mais um console.log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando com banco de dados")
+// Adicionando console.log:
+console.log("adicionando mais um console")
